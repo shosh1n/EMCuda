fb0405899f5e386951445ae037596beaa6256b84
shosh1n <hermannschris@googlemail.com>
2022-12-24 12:57:04 +0100

    Created Org-Readme file + makefile

---
+++ triangle.cpp
@@ -0,0 +1,10 @@
+/* Using standard C++ output libraries */
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/* Use glew.h instead of gl.h to get all the GL prototypes declared */
+#include <GL/gl.h>
+/* Using SDL2 for the base window and OpenGL context init */
+#include <SDL2/SDL.h>
+/* ADD GLOBAL VARIABLES HERE LATER */
