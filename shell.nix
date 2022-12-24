0b96c076968abbabc25f943124cd407b69558d83
shosh1n <hermannschris@googlemail.com>
2022-12-25 00:46:45 +0100

    Added glew.h

--- shell.nix
+++ shell.nix
@@ -6,7 +6,7 @@ pkgs.mkShell {
      git gitRepo gnupg autoconf curl
      procps gnumake util-linux m4 gperf unzip
      cudatoolkit linuxPackages.nvidia_x11
-     libGLU libGL SDL2
+     libGLU libGL SDL2 glew
      xorg.libXi xorg.libXmu freeglut
      xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
      ncurses5 stdenv.cc binutils
